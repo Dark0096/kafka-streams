96bc8a4476ec13e4fbbcdb341f38b7c40ca19c33
김성진 <kimseongjin@gimseongjin-ui-MacBookPro.local>
2019-06-06 22:40:19 +0900

    initialize project

---
+++ src/main/java/com/example/kafkastreamsexample/KafkaStreamsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.kafkastreamsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaStreamsExampleApplication.class, args);
+  }
+}
